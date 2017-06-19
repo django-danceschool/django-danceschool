573413aee1b3690268b58bd99ea2c402c6ee3067
Lee Tucker <lee@newschoolswing.com>
2017-06-19 18:08:54 -0400

    Fixed issue with Financial Detail View and event-related registration revenues causing 500 error.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-danceschool',
-    version='0.1.0-a',
+    version='0.1.0-b',
     packages=['danceschool'],
     include_package_data=True,
     license='BSD License',
