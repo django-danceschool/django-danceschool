fb5d6d06c3bdd8d8c93a814eb05c8e424642123f
Lee Tucker <lee.c.tucker@gmail.com>
2017-08-02 20:08:51 -0400

    Fixed bug where i18n template tag was not loaded for successful form submission template.

--- docs/version_history.rst
+++ docs/version_history.rst
@@ -1,6 +1,12 @@
 Version History
 ===============
 
+0.1.1
+-----
+
+- Fixed bug where email context was not being rendered for HTML emails
+- Fixed bug where i18n template tag was not loaded for successful form submission template.
+
 0.1.0
 -----
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-danceschool',
-    version='0.1.0-b3',
+    version='0.1.1',
     packages=['danceschool'],
     include_package_data=True,
     license='BSD License',
