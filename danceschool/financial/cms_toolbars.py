bc68042d80e1a95bdf82118d9d6f85ffd9b776d7
Lee Tucker <lee.c.tucker@gmail.com>
2017-08-12 18:39:31 -0400

    Fixed (I think) the position of the "Related Items" menu in the CMS toolbar Financial menu.

--- danceschool/financial/cms_toolbars.py
+++ danceschool/financial/cms_toolbars.py
@@ -82,7 +82,7 @@ class FinancialToolbar(CMSToolbar):
             self.request.user.has_perm('financial.change_expenseitem') or self.request.user.has_perm('financial.change_revenueitem') or
             self.request.user.has_perm('financial.change_expensecategory') or self.request.user.has_perm('financial.change_revenuecategory')
         ):
-            related_menu = menu.get_or_create_menu('financial-related',_('Related Items'), position=newPosition)
+            related_menu = menu.get_or_create_menu('financial-related',_('Related Items'), position=newPosition + 2)
 
             if self.request.user.has_perm('financial.change_expensecategory'):
                 related_menu.add_link_item(_('Expense Categories'), url=reverse('admin:financial_expensecategory_changelist'))
