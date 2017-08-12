c288c6dbb286f0c7823bbbd2730aa30933cb9069
Lee Tucker <lee.c.tucker@gmail.com>
2017-08-12 18:31:25 -0400

    Added list of customer-level overrides to prerequisite admin.

--- danceschool/prerequisites/admin.py
+++ danceschool/prerequisites/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.utils.html import format_html_join
 
 from danceschool.core.models import Customer
 from .models import Requirement, RequirementItem, CustomerRequirement
@@ -19,7 +21,7 @@ class CustomerRequirementInline(admin.StackedInline):
 @admin.register(Requirement)
 class RequirementAdmin(admin.ModelAdmin):
     inlines = [RequirementItemInline,]
-    readonly_fields = ['submissionDate','modifiedDate']
+    readonly_fields = ['submissionDate','modifiedDate','customerMeetsListing','customerDoesNotMeetListing']
     list_display = ['name','applicableLevel','applicableClass','enforcementMethod']
     list_editable = ['enforcementMethod',]
 
@@ -31,11 +33,40 @@ class RequirementAdmin(admin.ModelAdmin):
             'classes': ('collapse',),
             'fields': ('roleEnforced','applicableRole'),
         }),
+        (_('Customers Meeting Requirement'),{
+            'classes': ('collapse',),
+            'fields': ('customerMeetsListing','customerDoesNotMeetListing'),
+        }),
         (_('Date and Time'), {
             'classes': ('collapse',),
             'fields': ('submissionDate','modifiedDate'),
         })
     )
+
+    def customerMeetsListing(self,obj):
+        ''' Provide a list of customers meeting the requirement '''
+        return format_html_join(
+            '\n','<a href="{}">{}</a><br />',
+            [
+                (reverse('admin:core_customer_change', args=(customer.id,)),customer.fullName)
+                for customer in obj.customerrequirement_set.filter(met=True)
+            ]
+        )
+    customerMeetsListing.allow_tags = True
+    customerMeetsListing.short_description = _('Customers explicitly meeting requirement')
+
+    def customerDoesNotMeetListing(self,obj):
+        ''' Provide a list of customers meeting the requirement '''
+        return format_html_join(
+            '\n','<a href="{}">{}</a><br />',
+            [
+                (reverse('admin:core_customer_change', args=(customer.id,)),customer.fullName)
+                for customer in obj.customerrequirement_set.filter(met=False)
+            ]
+        )
+    customerDoesNotMeetListing.allow_tags = True
+    customerDoesNotMeetListing.short_description = _('Customers explicitly not meeting requirement')
+
 
 
 admin.site._registry[Customer].inlines.insert(0,CustomerRequirementInline)
