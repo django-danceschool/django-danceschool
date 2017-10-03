ca5683e9359b292f067837587d9509a74104fdc2
Lee Tucker <lee.c.tucker@gmail.com>
2017-10-03 18:01:19 -0400

    Fixed timezone issue with drop-in class labels.

--- danceschool/core/forms.py
+++ danceschool/core/forms.py
@@ -21,6 +21,7 @@ from .models import EventStaffMember, SubstituteTeacher, Event, EventOccurrence,
 from .constants import HOW_HEARD_CHOICES, getConstant, REG_VALIDATION_STR
 from .signals import check_student_info
 from .utils.emails import get_text_for_html
+from .utils.timezone import ensure_localtime
 
 # Define logger for this file
 logger = logging.getLogger(__name__)
@@ -246,11 +247,11 @@ class ClassChoiceForm(forms.Form):
             if isinstance(event,Series):
                 if event.allowDropins and user and user.has_perm('core.register_dropins'):
                     for occurrence in event.eventoccurrence_set.all():
-                        field_choices += ((json.dumps({'dropin_' + str(occurrence.id): True}), _('Drop-in: ') + occurrence.startTime.strftime('%B %-d')),)
+                        field_choices += ((json.dumps({'dropin_' + str(occurrence.id): True}), _('Drop-in: ') + ensure_localtime(occurrence.startTime).strftime('%B %-d')),)
                         self.permitted_event_keys.append('dropin_' + str(occurrence.id))
                 elif (user and user.has_perm('core.override_register_dropins')):
                     for occurrence in event.eventoccurrence_set.all():
-                        field_choices += ((json.dumps({'dropin_' + str(occurrence.id): True}),{'label': _('Drop-in: ') + occurrence.startTime.strftime('%B %-d'),'override':True}),)
+                        field_choices += ((json.dumps({'dropin_' + str(occurrence.id): True}),{'label': _('Drop-in: ') + ensure_localtime(occurrence.startTime).strftime('%B %-d'),'override':True}),)
                         self.permitted_event_keys.append('dropin_' + str(occurrence.id))
 
             self.fields['event_' + str(event.id)] = CheckboxSeriesChoiceField(
