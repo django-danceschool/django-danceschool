4dd1b0908b0be119819e78c1715485e52aad7c2a
Lee Tucker <lee.c.tucker@gmail.com>
2018-04-14 15:25:39 -0400

    Cannot order on properties.

--- danceschool/financial/admin.py
+++ danceschool/financial/admin.py
@@ -58,7 +58,7 @@ class ExpenseItemAdmin(admin.ModelAdmin):
     list_display = ('category','expenseStartDate','expenseEndDate','description','hours','total','approved','paid','reimbursement','payTo','paymentMethod')
     list_editable = ('approved','paid','paymentMethod')
     search_fields = ('description','comments','=category__name','=payToUser__first_name','=payToUser__last_name','=payToLocation__name')
-    list_filter = ('category','approved','paid','paymentMethod','reimbursement','payToLocation',('expenseStartDate',DateRangeFilter),('expenseEndDate',DateRangeFilter),('accrualDate',DateRangeFilter),('paymentDate',DateRangeFilter),('submissionDate',DateRangeFilter),'expenseRule')
+    list_filter = ('category','approved','paid','paymentMethod','reimbursement','payToLocation',('accrualDate',DateRangeFilter),('paymentDate',DateRangeFilter),('submissionDate',DateRangeFilter),'expenseRule')
     readonly_fields = ('submissionUser','expenseRule','expenseStartDate','expenseEndDate')
     actions = ('approveExpense','unapproveExpense')
 
