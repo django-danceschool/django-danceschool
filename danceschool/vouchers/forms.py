ff8dc7dca6dd142d4d7d9f7eee3d3cd4a57a52e6
Andrew Selzer <andrew.selzer@americanwell.com>
2019-05-07 14:50:58 -0400

    Code review feedback change to not break xgettext

--- danceschool/vouchers/forms.py
+++ danceschool/vouchers/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class VoucherCustomizationForm(forms.Form):
-
+    """ Form to customize vouchers for registration."""
     emailTo = forms.EmailField(label=_('Email gift certificate to'), required=True)
     emailType = forms.ChoiceField(
         label=_('This email belongs to'),
@@ -14,12 +14,10 @@ class VoucherCustomizationForm(forms.Form):
                     'If emailing to the purchaser, the gift message will only be included '
                     'as a PDF attachment.')
     )
-    recipientLabel = 'Recipient name (optional)'
-    recipientHelpText = 'If provided, this will be used to customize the gift message.'
     recipientName = forms.CharField(
-        label=_(recipientLabel),
+        label=_('Recipient name (optional)'),
         required=False,
-        help_text=_(recipientHelpText)
+        help_text=_('If provided, this will be used to customize the gift message.')
     )
     fromName = forms.CharField(label=_('From name (optional)'), required=False)
     message = forms.CharField(label=_('Enter a message to the recipient (optional)'), required=False)
