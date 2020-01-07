93e6d4b3790d0140e690583d8837d97ef819db27
Lee Tucker <lee.c.tucker@gmail.com>
2020-01-07 13:32:04 -0500

    Fixed path inheritance issue that broke the customer autocomplete

--- danceschool/door/urls.py
+++ danceschool/door/urls.py
@@ -4,7 +4,7 @@ from .views import DoorRegisterView
 from .autocomplete_light_registry import DoorRegisterAutoComplete
 
 urlpatterns = [
+    path('register/autocomplete/', DoorRegisterAutoComplete.as_view(), name='doorRegisterAutocomplete'),
     path('register/<slug:slug>/<int:year>/<int:month>/<int:day>/', DoorRegisterView.as_view(), name='doorRegister'),
     path('register/<slug:slug>/', DoorRegisterView.as_view(today=True), name='doorRegister'),
-    path('register/autocomplete/', DoorRegisterAutoComplete.as_view(), name='doorRegisterAutocomplete'),
 ]
