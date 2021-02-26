b8f1479abd0abd59b1e910e8aaaf55c29a9ddba6
Lee Tucker <lee.c.tucker@gmail.com>
2021-02-25 21:45:59 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-danceschool',
-    version='0.9.0',
+    version='0.9.1',
     packages=['danceschool'],
     include_package_data=True,
     license='BSD License',
