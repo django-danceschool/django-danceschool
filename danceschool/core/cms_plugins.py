d0b34dc4690c90280badcdee041f2ed4e1ddfbb0
Lee Tucker <lee.c.tucker@gmail.com>
2021-10-17 14:08:50 -0400

    Fix for #152, no instructors showing on Instructor pages.

--- danceschool/core/cms_plugins.py
+++ danceschool/core/cms_plugins.py
@@ -32,7 +32,7 @@ class StaffMemberListPlugin(PluginTemplateMixin, CMSPluginBase):
         listing = StaffMember.objects.all()
 
         if instance.statusChoices:
-            listing = listing.filter(instructor__status__in=instance.statusChoices)
+            listing = listing.filter(instructor__status__in=list(instance.statusChoices))
         else:
             listing = listing.exclude(instructor__status__in=[
                 Instructor.InstructorStatus.hidden,
