c03b76cabe4ec91bf20d643e7edb8989fb3a7100
Lee Tucker <lee.c.tucker@gmail.com>
2022-01-28 21:43:11 -0500

    Added monkeypatch to be able to edit static placeholders on the front end. See django-cms/django-cms#6433

--- danceschool/core/apps.py
+++ danceschool/core/apps.py
@@ -21,3 +21,25 @@ class CoreAppConfig(AppConfig):
         plugin_templates_registry.autodiscover(app_names)
         model_templates_registry.autodiscover(app_names)
         extras_templates_registry.autodiscover(app_names)
+
+        # See django-cms issue #6433. Hopefully this monkeypatch can be removed soon.
+        from cms.cms_toolbars import PageToolbar
+        from cms.utils import page_permissions
+
+        def monkeypatch_cms_has_publish_permission():
+            def has_publish_permission(self):
+                has_page = self.page is not None
+                publish_permission = False
+                if has_page:
+                    publish_permission = page_permissions.user_can_publish_page(
+                        self.request.user, page=self.page, site=self.current_site)
+                if (not has_page or publish_permission) and self.statics:
+                    publish_permission = all(
+                        sp.has_publish_permission(self.request)
+                        for sp in self.dirty_statics)
+
+                return publish_permission
+
+            return has_publish_permission
+
+        PageToolbar.has_publish_permission = monkeypatch_cms_has_publish_permission()
