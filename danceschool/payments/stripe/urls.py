e6d4f89e44dca7d2fd08529165e641cbd7324139
Artur Bajll <artur.bajll@orange.com>
2022-07-19 11:08:42 +0200

    Stripe integration refreshed

--- danceschool/payments/stripe/urls.py
+++ danceschool/payments/stripe/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import handle_stripe_checkout
+from .views import handle_stripe_checkout, create_checkout_session, SuccessView, CancelledView, webhook
 
 urlpatterns = [
     path('process/', handle_stripe_checkout, name='stripeHandler'),
+    path('create-checkout-session/', create_checkout_session, name='stripeSession'),
+    path('success/', SuccessView.as_view(), name='stripeSuccess'),
+    path('cancelled/', CancelledView.as_view(), name='stripeCanceled'),
+    path('webhook/', webhook, name='stripeWebhook'),
 ]
