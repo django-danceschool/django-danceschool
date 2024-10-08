django-danceschool/django-danceschool

commit 39fec9131ba39cde7f77b31ae4a65e843b6926ce

Updates for Django 4.2 compatibility.

danceschool/core/models.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementInclude import include
 from django.db import models
+from django.forms import MultipleChoiceField
 from django.contrib.auth.models import User, Group
 from django.urls import reverse
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
@@ -20,7 +21,6 @@ from datetime import datetime, timedelta
 from collections import Counter
 from filer.fields.image import FilerImageField
 from colorful.fields import RGBColorField
-from multiselectfield import MultiSelectField
 from calendar import month_name, day_name
 from math import ceil
 from itertools import accumulate
@@ -1417,7 +1417,7 @@ class Event(EmailRecipientMixin, PolymorphicModel):
         on its occcurrences.
         '''
         changed = False
-        occurrences = self.eventoccurrence_set.all()
+        occurrences = self.eventoccurrence_set.all() if self.pk else None
 
         if occurrences:
             new_year, new_month = self.getYearAndMonth()
@@ -1457,11 +1457,11 @@ class Event(EmailRecipientMixin, PolymorphicModel):
 
         startTime = (
             ensure_localtime(self.startTime) or
-            getattr(self.eventoccurrence_set.order_by('startTime').first(), 'startTime', None)
+            (getattr(self.eventoccurrence_set.order_by('startTime').first(), 'startTime', None) if self.pk else None)
         )
         endTime = (
             ensure_localtime(self.endTime) or
-            getattr(self.eventoccurrence_set.order_by('-endTime').first(), 'endTime', None)
+            (getattr(self.eventoccurrence_set.order_by('-endTime').first(), 'endTime', None) if self.pk else None)
         )
 
         # If set to these codes, then registration will be held closed
@@ -4474,10 +4474,10 @@ class StaffMemberListPluginModel(CMSPlugin):
         status = ('status', _('Instructor Status'))
         random = ('random', _('Randomly Ordered'))
 
-    statusChoices = MultiSelectField(
-        verbose_name=_('Limit to Instructors with Status'),
+    statusChoices = MultipleChoiceField(
+        label=_('Limit to Instructors with Status'),
         choices=Instructor.InstructorStatus.choices,
-        default=[
+        initial=[
             Instructor.InstructorStatus.roster,
             Instructor.InstructorStatus.assistant,
             Instructor.InstructorStatus.guest

danceschool/core/tests.py
@@ -48,7 +48,7 @@ class RegistrationTest(DefaultSchoolTestCase):
         self.assertEqual(s.registrationOpen, True)
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s, ])
         self.assertQuerysetEqual(response.context_data['regClosedSeries'], [])
 
     def test_past_series(self):
@@ -60,7 +60,7 @@ class RegistrationTest(DefaultSchoolTestCase):
         s = self.create_series()
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s, ])
         self.assertQuerysetEqual(response.context_data['regClosedSeries'], [])
 
         # Modify the existing class series to set the only eventoccurrence
@@ -105,7 +105,7 @@ class RegistrationTest(DefaultSchoolTestCase):
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context_data['regOpenSeries'], [])
-        self.assertQuerysetEqual(response.context_data['regClosedSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regClosedSeries'], [s, ])
         self.assertEqual(s.status, Event.RegStatus.enabled)
 
         # Delete the old occurrence, and check that registration opens back up
@@ -114,7 +114,7 @@ class RegistrationTest(DefaultSchoolTestCase):
         self.assertEqual(s.registrationOpen, True)
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s, ])
         self.assertQuerysetEqual(response.context_data['regClosedSeries'], [])
 
     def test_individual_class_page_visibility(self):
@@ -144,7 +144,7 @@ class RegistrationTest(DefaultSchoolTestCase):
 
         response = self.client.get(reverse('singleClassRegistration', args=(str(s.uuid),)))
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s, ])
 
         response = self.client.get(reverse('classView', args=(s.year, month_name[s.month], s.slug)))
         self.assertEqual(response.status_code, 404)
@@ -176,7 +176,7 @@ class RegistrationTest(DefaultSchoolTestCase):
 
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s.__repr__(), ])
+        self.assertQuerysetEqual(response.context_data['regOpenSeries'], [s, ])
 
         # Since this is an anonymous user, check that there is no option to register
         # at-the-door

danceschool/core/urls.py
@@ -17,8 +17,6 @@ from .autocomplete_light_registry import (
     EventAutoComplete, ClassDescriptionAutoComplete
 )
 
-admin.autodiscover()
-
 urlpatterns = [
     # These URLs are for Ajax and autocomplete functionality
     path('staff/substitute/filter/', updateSeriesAttributes, name='ajaxhandler_submitsubstitutefilter'),

danceschool/vouchers/views.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.urls import reverse
 
-from easy_pdf.views import PDFTemplateView
+from django_weasyprint import WeasyTemplateView
 import re
 import logging
 import random
@@ -138,8 +138,9 @@ class GiftCertificateCustomizeView(FormView):
         return HttpResponseRedirect(self.get_success_url())
 
 
-class GiftCertificatePDFView(PDFTemplateView):
+class GiftCertificatePDFView(WeasyTemplateView):
     template_name = 'vouchers/pdf/giftcertificate_template.html'
+    pdf_filename = 'gift_certificate.pdf'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

commit eefd33685dd22c71a363ff3699ddaf4e7d54ef2a

Fix staff member plugins (undid bug introduced in 39fec9131ba39cde7f77b31ae4a65e843b6926ce )

danceschool/core/models.py
@@ -1,6 +1,5 @@
 from xml.etree.ElementInclude import include
 from django.db import models
-from django.forms import MultipleChoiceField
 from django.contrib.auth.models import User, Group
 from django.urls import reverse
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
@@ -21,6 +20,7 @@ from datetime import datetime, timedelta
 from collections import Counter
 from filer.fields.image import FilerImageField
 from colorful.fields import RGBColorField
+from multiselectfield import MultiSelectField
 from calendar import month_name, day_name
 from math import ceil
 from itertools import accumulate
@@ -4474,14 +4474,14 @@ class StaffMemberListPluginModel(CMSPlugin):
         status = ('status', _('Instructor Status'))
         random = ('random', _('Randomly Ordered'))
 
-    statusChoices = MultipleChoiceField(
-        label=_('Limit to Instructors with Status'),
+    statusChoices = MultiSelectField(
+        verbose_name=_('Limit to Instructors with Status'),
         choices=Instructor.InstructorStatus.choices,
-        initial=[
+        default=[
             Instructor.InstructorStatus.roster,
             Instructor.InstructorStatus.assistant,
             Instructor.InstructorStatus.guest
-        ]
+        ],
     )
     orderChoice = models.CharField(_('Order By'), max_length=10, choices=OrderChoices.choices)
     imageThumbnail = models.ForeignKey(

setup.py
@@ -42,7 +42,7 @@ setup(
         'django-ical>=1.8.3',
         'django-imagekit>=4.1.0',
         'django-multi-email-field>=0.7.0',
-        'django-multiselectfield>=0.1.12',
+        'django-multiselectfield>=0.1.13',
         'django-polymorphic>=3.1.0',
         'django-sekizai>=3.0.1',
         'django-utils-six>=2.0',
@@ -59,6 +59,7 @@ setup(
         'huey>=2.4.4',
         'icalendar>=5.0.1',
         'intervaltree>=3.1.0',
+        'jsonschema>=4.23.0',
         'paypalrestsdk>=1.13.1',
         'persisting-theory>=1.0',
         'Pillow>=9.3.0',