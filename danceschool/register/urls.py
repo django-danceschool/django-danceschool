c7a786c16f0406598b8573a639062dab5805a7f0
Lee Tucker <lee.c.tucker@gmail.com>
2026-04-01 22:03:49 -0400

    Added back old register URL based on new location

--- danceschool/register/urls.py
+++ danceschool/register/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import PointOfSaleRegisterView
 from .autocomplete_light_registry import RegisterAutoComplete
+from danceschool.core.registration import PublicRegisterView
 
 urlpatterns = [
+    path('', PublicRegisterView.as_view(), name='registration'),
     path('autocomplete/', RegisterAutoComplete.as_view(), name='registerAutocomplete'),
     path('<slug:slug>/<int:year>/<int:month>/<int:day>/', PointOfSaleRegisterView.as_view(), name='registerView'),
     path('<slug:slug>/', PointOfSaleRegisterView.as_view(today=True), name='registerView'),
